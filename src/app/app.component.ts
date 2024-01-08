4e5ee19b313aeb361019180cec980a9a1ffe6eaf
ice-blaze <franketip@gmail.com>
2024-01-08 19:31:26 +0100

    Add error on purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,19 @@ import {FooterComponent} from "./footer/footer.component"
 	styleUrls: [`./app.component.scss`],
 })
 export class AppComponent {
+	public flag = false
+
+	public ngOnInit(): void {
+		setTimeout(
+			() => {
+				console.log(`hello`)
+
+				this.flag = true
+			},
+			5000,
+		)
+	}
+
 	public hello1(): void {
 		console.log()
 	}
