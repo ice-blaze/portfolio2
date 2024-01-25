ecb5f972244e47f24293f950c9f0e07fb3ca12db
ice-blaze <franketip@gmail.com>
2024-01-25 21:21:54 +0100

    ci: lighthouse minimum score

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,24 @@
 module.exports = {
   ci: {
-    upload: {
-      target: 'temporary-public-storage',
+    "onlyCategories": [
+      "accessibility",
+      "bestPractices",
+      "performance",
+      "seo",
+    ],
+    "assert": {
+      "assertions": {
+        "categories:accessibility": ["error", { "minScore": 1 }],
+        "categories:bestPractices": ["error", { "minScore": 1 }],
+        "categories:performance": ["error", { "minScore": 1 }],
+        "categories:seo": ["error", { "minScore": 1 }],
+      }
     },
-    collect: {
-      staticDistDir: "./dist/portfolio/browser/"
+    "upload": {
+      "target": 'temporary-public-storage',
+    },
+    "collect": {
+      "staticDistDir": "./dist/portfolio/browser/"
     }
   },
 };
