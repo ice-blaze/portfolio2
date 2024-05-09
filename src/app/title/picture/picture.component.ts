3f7ff56ee6f59562b87e152b55807496c706bf5d
ice-blaze <franketip@gmail.com>
2024-05-09 18:16:29 +0200

    Add fonts and empty picture component

---
+++ src/app/title/picture/picture.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+	selector: `app-picture`,
+	standalone: true,
+	imports: [],
+	templateUrl: `./picture.component.html`,
+	styleUrl: `./picture.component.scss`,
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class PictureComponent { }
